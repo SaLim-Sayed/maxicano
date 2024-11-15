469c804cbb041aecc192b62ffeb4bbf2229de0fa
Salem Mohamed <salem.mohamed@cibeg.com>
2024-11-15 17:15:52 +0200

    fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -48,7 +48,7 @@ export default async function RootLayout({
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NextUi>
             <MainNavbar />
-            <div className=" ">
+            <div className="bg-white ">
             <SwipperPage  />
               {children}
               
